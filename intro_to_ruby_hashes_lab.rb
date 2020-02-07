0c1ffb7c0e5f7637889cea82c19c7b186c5b5eb6
Ryan Seit <ryan@ryanseit.com>
2020-02-07 00:36:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,6 +26,9 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+  tricky_hash = {
+    :"#{key}" => "#{value}"
+  }
 end
 
 def read_from_hash(hash, key)
